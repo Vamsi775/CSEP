7525d45f204542f83f0af28c40201f1e7d0247dc
Vamsi775 <107165500+Vamsi775@users.noreply.github.com>
2022-06-09 11:59:23 +0530

    Delete average.py

--- average.py
+++
@@ -1,7 +0,0 @@
-import aptitude as m
-#addition
-a=eval(input("enter a value : "))
-b=eval(input("enter b value : "))
-c=eval(input("enter c  value : "))
-d=eval(input("enter d value : "))
-m.average(a,b,c,d)
